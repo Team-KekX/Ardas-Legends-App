7f60980d1c25c019fbd981d458113b81e16703e7
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-01-08 19:31:12 +0100

    Add Spring project
    Includes
    	- Maven Project (pom.xml)
    	- MavenWrapper
    	- Spring Application
    
    Signed-Off-By: Karim Jonas Gröller <karim.jonas.groeller@gmail.com>

---
+++ ALRoleplaySystem/src/main/java/com/ardaslegends/alroleplaysystem/AlRoleplaySystemApplication.java
@@ -0,0 +1,13 @@
+package com.ardaslegends.alroleplaysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlRoleplaySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlRoleplaySystemApplication.class, args);
+    }
+
+}
