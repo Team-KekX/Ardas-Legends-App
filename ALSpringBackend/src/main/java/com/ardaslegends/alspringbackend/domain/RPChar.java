e05503caba188d69ef9bc9f9437357561aa9e3c7
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-05-24 16:35:08 +0200

    Passed over RpChar-Entity
    Signed-Off-By: Karim Jonas Groeller <karim.jonas.groeller@gmail.com>

--- ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/RPChar.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/RPChar.java
@@ -1,16 +1,33 @@
 package com.ardaslegends.alspringbackend.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 
+@Entity
+@Table(name = "rpchars")
 public class RPChar {
 
-    private final String name; //unique, name of the character
-    private final Player player; //the player who this character belongs to
+    @Id
+    private String name; //unique, name of the character
+
+    @OneToOne(mappedBy = "rpChar")
+    private Player player; //the player who this character belongs to
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "current_region", foreignKey = @ForeignKey(name = "fk_current_region"))
+    @NotNull(message = "RpChar: currentRegion must not be null")
     private Region currentRegion; //the region the character is currently in
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "bound_to", foreignKey = @ForeignKey(name = "fk_bound_to"))
     private Army boundTo; //the army that is bound to this character
 
     public void setCurrentRegion(Region currentRegion) {
