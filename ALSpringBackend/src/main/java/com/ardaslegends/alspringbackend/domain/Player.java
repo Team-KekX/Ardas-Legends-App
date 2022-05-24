13675e27893bd2e7f32636d30c448f3ef63f0bd3
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-05-24 16:01:57 +0200

    Passed over Player-Entity
    Signed-Off-By: Karim Jonas Groeller <karim.jonas.groeller@gmail.com>

--- ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/Player.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/Player.java
@@ -3,14 +3,28 @@ package com.ardaslegends.alspringbackend.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 
+@Entity
+@Table(name = "players")
 public class Player {
 
+    @Id
     private String ign; //unique, ingame name of the player
+
+    @Column(name = "discord_id", unique = true)
     private String discordID; //unique, the ID of the player's discord account
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "faction", foreignKey = @ForeignKey(name = "fk_faction"))
+    @NotNull(message = "Player: Faction must not be null")
     private Faction faction; //the faction this character belongs to
+
+    @OneToOne(cascade = CascadeType.REMOVE, mappedBy = "player")
     private RPChar rpChar; //the player's rp character
 
 }
