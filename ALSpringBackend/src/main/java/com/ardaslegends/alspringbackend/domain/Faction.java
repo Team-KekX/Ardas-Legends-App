e08f57aacb6ed2afa87773aacd8dd5d1de723600
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-05-24 15:53:39 +0200

    Passed over Faction-Entity
    Signed-Off-By: Karim Jonas Groeller <karim.jonas.groeller@gmail.com>

--- ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/Faction.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/Faction.java
@@ -5,9 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Getter
@@ -21,12 +19,20 @@ public class Faction {
 
     @Id
     private String name; //unique, name of the faction
+
+    @OneToOne
     private Player leader; //the player who leads this faction
+
+    @OneToMany(mappedBy = "faction")
     private List<Army> armies; //all current armies of this faction
+    @OneToMany(mappedBy = "faction")
     private List<Player> players; //all current players of this faction
+    @ManyToMany(mappedBy = "claimedBy")
     private List<Region> regions; //all regions this faction claims
+    @OneToMany(mappedBy = "ownedBy")
     private List<ClaimBuild> claimBuilds; //all claimbuilds of this faction
-    private final String colorcode; //the faction's colorcode, used for painting the map
+    
+    private String colorcode; //the faction's colorcode, used for painting the map
 
     public void setLeader(Player leader) {
         this.leader = leader;
