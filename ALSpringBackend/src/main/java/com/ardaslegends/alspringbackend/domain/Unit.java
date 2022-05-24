b673e72fa5eeb40a9f64797f679b629a20984f1c
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-05-24 16:44:53 +0200

    Passed over Unit Entities
    Signed-Off-By: Karim Jonas Groeller <karim.jonas.groeller@gmail.com>

--- ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/Unit.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/Unit.java
@@ -1,14 +1,27 @@
 package com.ardaslegends.alspringbackend.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 
+@Embeddable
 public class Unit {
 
-    private final UnitType unitType; //The kind of unit, e.g. Gondor Soldier
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "unit_type", foreignKey = @ForeignKey(name = "fk_unit_type"))
+    private UnitType unitType; //The kind of unit, e.g. Gondor Soldier
     private Integer count; //how many of those units are in the army
 
     public void setCount(Integer count) {
--- ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/UnitType.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/UnitType.java
@@ -1,14 +1,27 @@
 package com.ardaslegends.alspringbackend.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 
+@Entity
+@Table(name = "unit_types")
 public class UnitType {
 
-    private final String unitName; //unique, the name of this Unit
-    private final Integer tokenCost; //how much tokens this unit costs
+    @Id
+    private String unitName; //unique, the name of this Unit
+    @NotNull(message = "UnitType: tokenCost must not be null")
+    private Integer tokenCost; //how much tokens this unit costs
 
 }
