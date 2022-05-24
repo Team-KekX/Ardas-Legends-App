f6a89caaddc84009ff500130d5a30846b4562ccf
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-05-24 16:54:14 +0200

    Passed over ProductionSite-Entity
    Signed-Off-By: Karim Jonas Groeller <karim.jonas.groeller@gmail.com>

--- ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/ProductionSite.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/ProductionSite.java
@@ -1,16 +1,29 @@
 package com.ardaslegends.alspringbackend.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 
+@Entity
+@Table(name = "production_sites")
 public class ProductionSite {
 
-    private final Long id;
-    private final ProductionSiteType type; //unique, type of production site, e.g. FARM
-    private final String producedResource; //the resource this production site produces
-    private final Integer amount; //the amount
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ProductionSiteType type; //unique, type of production site, e.g. FARM
+
+    private String producedResource; //the resource this production site produces
+    private Integer amount; //the amount
 
 }
