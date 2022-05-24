dd1bd2beabf1cc16c35471af22214cb4e85a4a4c
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-05-24 15:39:04 +0200

    Finished Claimbuild-Entity
    Signed-Off-By: Karim Jonas Groeller <karim.jonas.groeller@gmail.com>

--- ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/ClaimBuild.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/alspringbackend/domain/ClaimBuild.java
@@ -3,24 +3,60 @@ package com.ardaslegends.alspringbackend.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
 
+@Entity
+@Table(name = "claimbuilds")
 public class ClaimBuild {
+    @Id
     private String name; //unique, name of the claimbuild
-    private String region; //the Id of the region the claimbuild is in
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "region", foreignKey = @ForeignKey(name = "fk_region"))
+    @NotNull(message = "Claimbuild: Region must not be null")
+    private Region region; //the region the claimbuild is in
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "Claimbuild: ClaimbuildType must not be null")
     private ClaimBuildType type; //Type of claimbuild, e.g. HAMLET
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "owned_by", foreignKey = @ForeignKey(name = "fk_owned_by"))
+    @NotNull(message = "Claimbuild: ownedBy must not be null")
     private Faction ownedBy; //faction which owns this CB
+
+    @Embedded
+    @NotNull(message = "Claimbuild: Coordinate must not be null")
     private Coordinate coordinates; //coordinate locations
+
+    @OneToMany(mappedBy = "stationedAt")
     private List<Army> stationedArmies; //armies which are stationed in this CB
+
+    @OneToMany(mappedBy = "originalClaimbuild")
     private List<Army> createdArmies; //armies which were created from this CB. Usually only 1 army, but capitals can create 2
+
+    @OneToMany(mappedBy = "originalClaimbuild")
     private List<Army> createdTradingCompanies; //TCs which were created from this CB. Seperated from armies so you can search for them more easily.
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "production_sites", foreignKey = @ForeignKey(name = "fk_production_sites"))
     private List<ProductionSite> productionSites; //the production sites in this cb
+
+    @ElementCollection(targetClass = SpecialBuilding.class)
+    @Column(name = "special_buildings")
+    @Enumerated(EnumType.STRING)
     private List<SpecialBuilding> specialBuildings; //special buildings in this cb, e.g. House of Healing
+
     private String traders; //traders in this CB. e.g. Dwarven Smith. Only relevant for staff so they know which traders need to be added
+
     private String numberOfHouses; //houses in this CB, e.g. 4 large 12 small. Only relevant for staff
+
+    @OneToMany
     private List<Player> builtBy; //the player who built the CB
 
     public void setType(ClaimBuildType type) {
