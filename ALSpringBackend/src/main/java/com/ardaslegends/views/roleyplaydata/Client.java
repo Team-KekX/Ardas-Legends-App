c9d7fdfafad01a9a92b5a14abb14e56eb4afcc01
Luktronic <luka.furundzija@gmx.at>
2022-05-25 12:42:06 +0200

    Migrated to Vaadin Project
    Changed the spring backend project to a generated default Vaadin Project
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

---
+++ ALSpringBackend/src/main/java/com/ardaslegends/views/roleyplaydata/Client.java
@@ -0,0 +1,59 @@
+package com.ardaslegends.views.roleyplaydata;
+
+public class Client {
+
+    private int id;
+    private String img;
+    private String client;
+    private double amount;
+    private String status;
+    private String date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
