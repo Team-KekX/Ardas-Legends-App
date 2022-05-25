cd8d9fa44b222465e3e79a81ccf185081b473e6c
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-05-25 15:05:36 +0200

    Updated Unit and Sieges Mapping in Army

--- ALSpringBackend/src/main/java/com/ardaslegends/data/domain/Army.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/domain/Army.java
@@ -40,10 +40,12 @@ public class Army {
     @JoinColumn(name = "bound_to", foreignKey = @ForeignKey(name = "fk_bound_to"))
     @NotNull(message = "Army: boundTo must not be null")
     private RPChar boundTo; //rp character the army is currently bound to
-    @OneToMany
+    @OneToMany(mappedBy = "army")
     @NotEmpty(message = "Army: units must not be empty")
     private List<Unit> units; //the units in this army contains
     @ElementCollection
+    @CollectionTable(name = "army_sieges",
+                joinColumns = @JoinColumn(name = "army_id", foreignKey = @ForeignKey(name = "fk_army_id")))
     private List<String> sieges; //list of siege equipment this
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "stationed_at", foreignKey = @ForeignKey(name = "fk_stationed_at"))
--- ALSpringBackend/src/main/java/com/ardaslegends/data/domain/Unit.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/domain/Unit.java
@@ -23,7 +23,13 @@ public class Unit {
     @ManyToOne
     @JoinColumn(name = "unit_type", foreignKey = @ForeignKey(name = "fk_unit_type"))
     private UnitType unitType; //The kind of unit, e.g. Gondor Soldier
+
+    @ManyToOne
+    @JoinColumn(name = "army", foreignKey = @ForeignKey(name = "fk_army"))
+    private Army army; // The army in which these units are
+
     private Integer count; //maximum aamount of those units that are in the army
+
     private Integer amountAlive; //current alive soldiers
 
 
