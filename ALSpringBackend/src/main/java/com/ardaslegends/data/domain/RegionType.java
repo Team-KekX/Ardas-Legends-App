41315f23f4a168fb69c6760130180e341dc4cb9e
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-05-30 17:40:18 +0200

    Added Cost of Movement for RegionTypes
    Signed-Off-By: Karim Jonas Groeller <karim.jonas.groeller@gmail.com>

--- ALSpringBackend/src/main/java/com/ardaslegends/data/domain/RegionType.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/domain/RegionType.java
@@ -1,13 +1,25 @@
 package com.ardaslegends.data.domain;
 
 public enum RegionType {
-    SEA,
-    LAND,
-    MOUNTAIN,
-    FOREST,
-    JUNGLE,
-    DESERT,
-    ICE,
-    SWAMP,
-    HILL
+
+    // The number next to the Enum represensts the cost/duration to move into such a region
+
+    SEA(1),
+    LAND(1),
+    MOUNTAIN(1),
+    FOREST(1),
+    JUNGLE(1),
+    DESERT(1),
+    ICE(1),
+    SWAMP(1),
+    HILL(1);
+
+    private final int cost;
+    private RegionType(int cost){
+        this.cost = cost;
+    }
+
+    public int getCost() {
+        return cost;
+    }
 }
