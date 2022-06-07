e9f942dac36a7932de48908cd1865d6d0b8d310f
Alex Stergiou <jutaroroche@gmail.com>
2022-06-07 19:49:59 +0200

    Ping group roles in peace declaration

--- Bot/commands/subcommands/declare/declare_peace.js
+++ Bot/commands/subcommands/declare/declare_peace.js
@@ -2,11 +2,14 @@ const {capitalizeFirstLetters} = require("../../../utils/utilities");
 
 module.exports = {
     async execute(interaction) {
-        const attacker=capitalizeFirstLetters(interaction.options.getString('attacker-faction').toLowerCase());
-        const defender=capitalizeFirstLetters(interaction.options.getString('defender-faction').toLowerCase());
+        const attacker = capitalizeFirstLetters(interaction.options.getString('attacker-faction').toLowerCase());
+        const defender = capitalizeFirstLetters(interaction.options.getString('defender-faction').toLowerCase());
         const guild = interaction.client.guilds.cache.get("926452447243825192");
         const attacker_role = guild.roles.cache.find(role => role.name === `${attacker}`);
         const defender_role = guild.roles.cache.find(role => role.name === `${defender}`);
-        await interaction.reply(`<@&${attacker_role.id}> made peace with <@&${defender_role.id}>.`);
+        interaction.reply({
+            allowedMentions: {roles: [attacker_role.id, defender_role.id]},
+            content: `<@&${attacker_role.id}> made peace with <@&${defender_role.id}>.`
+        })
     },
 };
