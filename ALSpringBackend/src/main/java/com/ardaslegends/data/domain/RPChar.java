60f7e3022fdf1378aab2b4e269ceaaffc3afc66a
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-06-13 16:52:36 +0200

    Make RPChar.name UNIQUE

--- ALSpringBackend/src/main/java/com/ardaslegends/data/domain/RPChar.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/domain/RPChar.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotNull;
 @Embeddable
 public class RPChar {
 
+    @Column(unique = true)
     private String name;
 
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
