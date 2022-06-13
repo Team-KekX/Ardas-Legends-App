93dac91fa093d89b71a41749314e9ae9e57dead4
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-06-13 16:32:55 +0200

    Changed RPChar, Player Relation
    - RPChar is now an embeddable of Player
    - Changed Army.boundTo relation so that it complies with the above
      changes
    
    Signed-Off-By: Karim Jonas Groeller <karim.jonas.groeller@gmail.com>

--- ALSpringBackend/src/main/java/com/ardaslegends/data/domain/RPChar.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/domain/RPChar.java
@@ -11,22 +11,18 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 @Builder
 
-@Entity
-@Table(name = "rpchars")
+@Embeddable
 public class RPChar {
 
-    @Id
-    private String name; //unique, name of the character
+    private String name;
 
-    @OneToOne(mappedBy = "rpChar")
-    private Player player; //the player who this character belongs to
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "current_region", foreignKey = @ForeignKey(name = "fk_current_region"))
     @NotNull(message = "RpChar: currentRegion must not be null")
     private Region currentRegion; //the region the character is currently in
-    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+
+    @OneToOne
     @JoinColumn(name = "bound_to", foreignKey = @ForeignKey(name = "fk_bound_to"))
     private Army boundTo; //the army that is bound to this character
-
 
 }
