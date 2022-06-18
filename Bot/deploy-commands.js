53d18fb4c6f0337e14dc17df0047574d2108f6dc
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-06-18 15:20:26 +0200

    Merge Feature#007/bot-advanced-features
        Add gitignore back to base directory
        Commented notifyStaff and rpapp reaction count
        Base code to count rp app reaction, not functional
        Removed embark/disembark. Merged settle/visit/station commands
        Moved Bot config files to their corresponding folder
        Added admin commands category. Added an RP channel setup.
        Ping group roles in peace declaration
        Ping group roles in war declarations

--- Bot/deploy-commands.js
+++ Bot/deploy-commands.js
@@ -6,15 +6,21 @@ const { clientId, guildId, token } = require('./configs/config.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./Bot/commands').filter(file => file.endsWith('.js'));
+const adminCommandFiles = fs.readdirSync('./Bot/commands/admin').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     commands.push(command.data.toJSON());
 }
 
+for (const file of adminCommandFiles) {
+    const command = require(`./commands/admin/${file}`);
+    commands.push(command.data.toJSON());
+}
+
 
 // noinspection JSCheckFunctionSignatures,JSClosureCompilerSyntax
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({version: '9'}).setToken(token);
 
 
 (async () => {
--- Bot/events/messageReactionAdd.js
+++ Bot/events/messageReactionAdd.js
@@ -1,7 +1,41 @@
+const {rpAppsChannelID, upvoteEmoji, downvoteEmoji, staffRoles, staffMajorityCount} = require("../configs/config.json");
+
+function countRPAppReactions(reaction, user) {
+    if (reaction.message.channel.id === rpAppsChannelID) {
+        // Now the message has been cached and is fully available
+        console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+        // The reaction is now also fully available and the properties will be reflected accurately:
+        console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+
+        if ([upvoteEmoji, downvoteEmoji].includes(reaction.emoji.name)) {
+            // If the reaction is an upvote or downvote, check if it's by staff
+            for (const role of staffRoles) {
+                if (user.roles.cache.has(role)) {
+                    // If the user is staff, check reaction count
+                    const upvotes = reaction.message.reactions.cache.get(upvoteEmoji).count;
+                    const downvotes = reaction.message.reactions.cache.get(downvoteEmoji).count;
+
+                    if (upvotes + downvotes >= staffMajorityCount) {
+                        // App has the necessary votes. Send result to player and if it's approved send to server
+                        if (upvotes > downvotes) {
+                            console.log("Approved!");
+                        } else {
+                            console.log("Denied!");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
+
 
 module.exports = {
     name: 'messageReactionAdd',
     async execute(reaction, user) {
+        // Check if it's the bot which added the reaction
+        if (reaction.me === true) return;
         // When a reaction is received, check if the structure is partial
         if (reaction.partial) {
             // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
@@ -13,10 +47,7 @@ module.exports = {
                 return;
             }
         }
+        //countRPAppReactions(reaction, user);
 
-        // Now the message has been cached and is fully available
-        console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
-        // The reaction is now also fully available and the properties will be reflected accurately:
-        console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
     },
 };
