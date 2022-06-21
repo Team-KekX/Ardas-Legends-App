3f076daa994b9b291a0a009d9186b8175c1304fd
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-06-21 09:54:34 +0200

    Deleted unnecessary TestFactionRepository

--- ALSpringBackend/src/main/java/com/ardaslegends/data/repository/TestFactionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ardaslegends.data.repository;
-
-import com.ardaslegends.data.domain.Faction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestFactionRepo extends JpaRepository<Faction, String> {
-}
