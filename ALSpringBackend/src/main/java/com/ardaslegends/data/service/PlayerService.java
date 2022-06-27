7a83e99479e013ad51dc4caa1d567ec1a09e2829
Luktronic <luka.furundzija@gmx.at>
2022-06-28 00:49:06 +0200

    Added moveRpChar()
    Implemented the moveRpChar() method.
    The function creates a new movement record in the database for the
    desired movement.
    Tests still need to be written
    
     Changes to be committed:
    	modified:   ALSpringBackend/src/main/java/com/ardaslegends/data/service/PlayerService.java
    	new file:   ALSpringBackend/src/main/java/com/ardaslegends/data/service/dto/player/rpchar/MoveRpCharDto.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- ALSpringBackend/src/main/java/com/ardaslegends/data/service/PlayerService.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/service/PlayerService.java
@@ -1,11 +1,11 @@
 package com.ardaslegends.data.service;
 
-import com.ardaslegends.data.domain.Faction;
-import com.ardaslegends.data.domain.Player;
-import com.ardaslegends.data.domain.RPChar;
+import com.ardaslegends.data.domain.*;
+import com.ardaslegends.data.repository.MovementRepository;
 import com.ardaslegends.data.repository.PlayerRepository;
 import com.ardaslegends.data.service.dto.player.*;
 import com.ardaslegends.data.service.dto.player.rpchar.CreateRPCharDto;
+import com.ardaslegends.data.service.dto.player.rpchar.MoveRpCharDto;
 import com.ardaslegends.data.service.dto.player.rpchar.UpdateRpCharDto;
 import com.ardaslegends.data.service.dto.player.rpchar.UpdateRpCharNameDto;
 import com.ardaslegends.data.service.exceptions.ServiceException;
@@ -18,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.PersistenceException;
 import java.lang.reflect.Field;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -36,9 +37,12 @@ import java.util.stream.Collectors;
 public class PlayerService extends AbstractService<Player, PlayerRepository> {
 
     private final PlayerRepository playerRepository;
-    private final FactionService factionService;
 
+    private final FactionService factionService;
+    private final RegionService regionService;
+    private final MovementService movementService;
     private final MojangApiService mojangApiService;
+    private final Pathfinder pathfinder;
 
     @Transactional(readOnly = false)
     public Player createPlayer(CreatePlayerDto dto) {
@@ -496,4 +500,52 @@ public class PlayerService extends AbstractService<Player, PlayerRepository> {
         log.info("Succesfully deleted rpchar [{}]", deletedRpChar);
         return deletedRpChar;
     }
+
+    //TODO: Write Test
+    @Transactional(readOnly = false)
+    public RPChar moveRpChar(MoveRpCharDto dto) {
+        log.debug("Moving RpChar of player {} to Region {}", dto.discordId(), dto.toRegion());
+
+        //Validating data
+
+        log.trace("Validating Data");
+        ServiceUtils.checkAllNulls(dto);
+        ServiceUtils.checkAllBlanks(dto);
+
+        log.trace("Getting the player");
+        Player player = getPlayerByDiscordId(dto.discordId());
+
+        log.trace("Setting the RPChar");
+        RPChar rpChar = player.getRpChar();
+
+        log.debug("Checking if the Player has a RP Char");
+        if(rpChar == null) {
+            log.warn("Player {} has no RP Char!", player);
+            throw new IllegalArgumentException("You have no RP Char!");
+        }
+
+        //Setting up Region Data
+
+        log.trace("Find the region the player is moving to");
+        Region toRegion = regionService.getRegionById(dto.toRegion());
+
+        log.trace("Getting the RPChar's current region");
+        Region fromRegion = rpChar.getCurrentRegion();
+
+        log.debug("Calling the pathfinder to find the fastest way from '{}' -> '{}'", fromRegion.getId(), toRegion.getId());
+        Path shortestPath = pathfinder.findShortestWay(fromRegion, toRegion, player, true);
+
+        log.trace("Getting the current time");
+        LocalDateTime currentTime = LocalDateTime.now();
+
+        log.trace("Building the movement object");
+        Movement movement = Movement.builder().player(player).path(shortestPath).startTime(currentTime).endTime(currentTime.plusDays(shortestPath.getCost())).isCharMovement(true).isAccepted(false).build();
+
+        log.trace("Saving the new movement");
+        movementService.createMovement(movement);
+
+        log.info("Successfully created new Movement for the RPChar '{}' of Player '{}'", rpChar.getName(), player);
+        return rpChar;
+    }
+
 }
