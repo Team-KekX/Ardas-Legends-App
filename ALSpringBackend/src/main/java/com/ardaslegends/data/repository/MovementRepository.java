85980cb3d8f0c2a4d38627db1e2e62b1a45b8e4d
Luktronic <luka.furundzija@gmx.at>
2022-06-28 00:21:04 +0200

    Added empty MovementRepository
    Added empty MovementRepository so I can save movements into the database
    
     Changes to be committed:
    	new file:   ALSpringBackend/src/main/java/com/ardaslegends/data/repository/MovementRepository.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

---
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/repository/MovementRepository.java
@@ -0,0 +1,10 @@
+package com.ardaslegends.data.repository;
+
+import com.ardaslegends.data.domain.Movement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovementRepository extends JpaRepository<Movement, Long> {
+
+}
