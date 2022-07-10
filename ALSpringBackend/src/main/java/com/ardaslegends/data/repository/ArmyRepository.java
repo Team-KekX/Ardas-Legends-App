d006ffac4a3772f52cbbb190c25c367deadec4d7
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-07-10 12:38:59 +0200

    ArmyRepository countArmiesByClaimbuild Query

--- ALSpringBackend/src/main/java/com/ardaslegends/data/repository/ArmyRepository.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/repository/ArmyRepository.java
@@ -1,6 +1,7 @@
 package com.ardaslegends.data.repository;
 
 import com.ardaslegends.data.domain.Army;
+import com.ardaslegends.data.domain.ClaimBuild;
 import com.ardaslegends.data.service.ArmyService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,6 @@ import java.util.Optional;
 public interface ArmyRepository extends JpaRepository<Army, String> {
 
     public Optional<Army> findArmyByName(String name);
+
+    int countArmiesByOriginalClaimbuild(ClaimBuild claimBuild);
 }
