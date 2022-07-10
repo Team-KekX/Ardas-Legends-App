0d4413c2fe074063bfa6846262bc449e3053d09b
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-07-10 11:39:09 +0200

    Add maxArmies&companies as well as freeArmies&companies in ClaimbuildType

--- ALSpringBackend/src/main/java/com/ardaslegends/data/domain/ClaimBuildType.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/domain/ClaimBuildType.java
@@ -1,11 +1,25 @@
 package com.ardaslegends.data.domain;
 
 public enum ClaimBuildType {
-    HAMLET,
-    VILLAGE,
-    TOWN,
-    CAPITAL,
-    KEEP,
-    CASTLE,
-    STRONGHOLD
+    HAMLET(0,0,0,0),
+    VILLAGE(0,0,0,0),
+    TOWN(1,1,1,1),
+    CAPITAL(2,1,2,1),
+    KEEP(0,0,0,0),
+    CASTLE(1,0,1,0),
+    STRONGHOLD(1,0,1,0);
+
+    private final int maxArmies;
+    private final int freeArmies;
+    private final int maxTradingCompanies;
+    private final int freeTradingCompanies;
+    private ClaimBuildType(int maxArmies, int maxTradingCompanies, int freeArmies, int freeTradingCompanies) {
+        this.maxArmies = maxArmies;
+        this.maxTradingCompanies = maxTradingCompanies;
+        this.freeArmies = freeArmies;
+        this.freeTradingCompanies = freeTradingCompanies;
+    }
+
+    public int getFreeArmies() {return freeArmies; }
+    public int getFreeTradingCompanies() {return freeTradingCompanies; }
 }
