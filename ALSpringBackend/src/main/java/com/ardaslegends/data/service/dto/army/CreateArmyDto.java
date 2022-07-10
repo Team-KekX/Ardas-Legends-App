3a34c7ddcbd28da1f95eb9bf3fe380e059a80b6a
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-07-10 14:47:35 +0200

    Adding Type to CreateArmyDto

--- ALSpringBackend/src/main/java/com/ardaslegends/data/service/dto/army/CreateArmyDto.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/service/dto/army/CreateArmyDto.java
@@ -1,6 +1,7 @@
 package com.ardaslegends.data.service.dto.army;
 
+import com.ardaslegends.data.domain.ArmyType;
 import com.ardaslegends.data.service.dto.unit.UnitTypeDto;
 
-public record CreateArmyDto(String name, String faction, String claimBuildName, UnitTypeDto[] units) {
+public record CreateArmyDto(String name, String faction, ArmyType armyType, String claimBuildName, UnitTypeDto[] units) {
 }
