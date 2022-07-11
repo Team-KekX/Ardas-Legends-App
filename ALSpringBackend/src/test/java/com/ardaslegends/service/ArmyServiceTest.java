05754b741b5d07279c0bbe2e28b4020dd202b0bc
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-07-11 22:54:50 +0200

    Added some tests for createArmy

--- ALSpringBackend/src/test/java/com/ardaslegends/service/ArmyServiceTest.java
+++ ALSpringBackend/src/test/java/com/ardaslegends/service/ArmyServiceTest.java
@@ -9,6 +9,8 @@ import com.ardaslegends.data.service.ClaimBuildService;
 import com.ardaslegends.data.service.PlayerService;
 import com.ardaslegends.data.service.UnitTypeService;
 import com.ardaslegends.data.service.dto.army.BindArmyDto;
+import com.ardaslegends.data.service.dto.army.CreateArmyDto;
+import com.ardaslegends.data.service.dto.unit.UnitTypeDto;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +18,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -40,6 +44,62 @@ public class ArmyServiceTest {
         armyService = new ArmyService(mockArmyRepository, mockPlayerService, mockFactionRepository, mockUnitTypeService, claimBuildRepository);
     }
 
+    // Create Army
+    @Test
+    void ensureCreateArmyWorksProperly() {
+
+    }
+
+    @Test
+    void ensureCreateArmyThrowsIAEWhenArmyNameIsAlreadyTaken() {
+        log.debug("Testing if createArmy correctly throws IAE when name is already taken");
+
+        log.trace("Initializing data");
+        CreateArmyDto dto = new CreateArmyDto("Kek", "Kek", ArmyType.ARMY, "Kek", new UnitTypeDto[]{new UnitTypeDto("Kek", 10)});
+
+        when(mockArmyRepository.findById(dto.name())).thenReturn(Optional.of(new Army()));
+
+        log.debug("Expecting IAE on call");
+        log.debug("Calling createArmy()");
+        var result = assertThrows(IllegalArgumentException.class, () -> armyService.createArmy(dto));
+
+        assertThat(result.getMessage()).contains("already exists");
+        log.info("Test passed: IAE when Army Name is taken!");
+    }
+    @Test
+    void ensureCreateArmyThrowsIAEWhenNoValidFactionFound() {
+        log.debug("Testing if createArmy correctly throws IAE when no valid faction could be found");
+
+        log.trace("Initializing data");
+        CreateArmyDto dto = new CreateArmyDto("Kek", "Kek", ArmyType.ARMY, "Kek", new UnitTypeDto[]{new UnitTypeDto("Kek", 10)});
+
+        when(mockArmyRepository.findById(dto.name())).thenReturn(Optional.empty());
+        when(mockFactionRepository.findById(dto.faction())).thenReturn(Optional.empty());
+
+        log.debug("Expecting IAE on call");
+        log.debug("Calling createArmy()");
+        var result = assertThrows(IllegalArgumentException.class, () -> armyService.createArmy(dto));
+
+        assertThat(result.getMessage()).contains("No faction found");
+    }
+    @Test
+    void ensureCreateArmyThrowsIAEWhenNoClaimBuildWithInputNameHasBeenFound() {
+        log.debug("Testing if createArmy correctly throws IAE when no claimBuild could be found");
+
+        log.trace("Initializing data");
+        CreateArmyDto dto = new CreateArmyDto("Kek", "Kek", ArmyType.ARMY, "Kek", new UnitTypeDto[]{new UnitTypeDto("Kek", 10)});
+
+        when(mockArmyRepository.findById(dto.name())).thenReturn(Optional.empty());
+        when(mockFactionRepository.findById(dto.faction())).thenReturn(Optional.of(new Faction()));
+        when(mockUnitTypeService.getUnitTypeByName(any())).thenReturn(new UnitType("Kek", 1.0));
+        when(claimBuildRepository.findById(dto.claimBuildName())).thenReturn(Optional.empty());
+
+        log.debug("Expecting IAE on call");
+        log.debug("Calling createArmy()");
+        var result = assertThrows(IllegalArgumentException.class, () -> armyService.createArmy(dto));
+
+        assertThat(result.getMessage()).contains("No ClaimBuild found");
+    }
     @Test
     void ensureBindWorksWhenBindingSelf() {
         log.debug("Testing if army binding works properly!");
