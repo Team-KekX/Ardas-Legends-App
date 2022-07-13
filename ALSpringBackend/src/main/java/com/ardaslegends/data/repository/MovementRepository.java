9682acc79e2a37a07a4db257390a37060ddc6031
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-07-13 21:03:54 +0200

    Implemented ArmyRepository Queries
    - findByArmyAndIsCurrentlyActiveTrue
    - findByPlayerAndIsCurrentlyActiveTrue

--- ALSpringBackend/src/main/java/com/ardaslegends/data/repository/MovementRepository.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/repository/MovementRepository.java
@@ -1,5 +1,6 @@
 package com.ardaslegends.data.repository;
 
+import com.ardaslegends.data.domain.Army;
 import com.ardaslegends.data.domain.Movement;
 import com.ardaslegends.data.domain.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,6 @@ import java.util.Optional;
 public interface MovementRepository extends JpaRepository<Movement, Long> {
     //TODO add Test
     public List<Movement> findMovementsByPlayer(Player player);
+    public Optional<Movement> findMovementByArmyAndIsCurrentlyActiveTrue(Army army);
+    public Optional<Movement> findMovementByPlayerAndIsCurrentlyActiveTrue(Player player);
 }
