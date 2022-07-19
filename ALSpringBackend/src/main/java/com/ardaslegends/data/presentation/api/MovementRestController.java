bb1cdbe01a7fea50448e02f4f55f08dfeba40745
Luktronic <59698507+Luktronic@users.noreply.github.com>
2022-07-19 17:18:51 +0200

    Added test for createArmyMovement request
     Changes to be committed:
    	modified:   ALSpringBackend/src/main/java/com/ardaslegends/data/presentation/api/MovementRestController.java
    	modified:   ALSpringBackend/src/test/java/com/ardaslegends/presentation/api/MovementRestControllerTest.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- ALSpringBackend/src/main/java/com/ardaslegends/data/presentation/api/MovementRestController.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/presentation/api/MovementRestController.java
@@ -3,6 +3,7 @@ package com.ardaslegends.data.presentation.api;
 import com.ardaslegends.data.domain.Movement;
 import com.ardaslegends.data.presentation.AbstractRestController;
 import com.ardaslegends.data.service.MovementService;
+import com.ardaslegends.data.service.dto.army.MoveArmyDto;
 import com.ardaslegends.data.service.dto.player.DiscordIdDto;
 import com.ardaslegends.data.service.dto.player.rpchar.MoveRpCharDto;
 import lombok.RequiredArgsConstructor;
@@ -23,13 +24,14 @@ public class MovementRestController extends AbstractRestController {
     public static final String BASE_URL = "/api/movement";
 
     public static final String PATH_MOVE_CHAR = "/move-char";
+    public static final String PATH_MOVE_ARMY = "/move-army";
     public static final String PATH_CANCEL_CHAR_MOVEMENT = "/cancel-char-move";
 
     @PostMapping(PATH_MOVE_CHAR)
     public HttpEntity<Movement> moveRoleplayCharacter(@RequestBody MoveRpCharDto dto) {
 
         log.debug("Incoming Post Request to create rp char movement, data [{}]", dto);
-        log.trace("WrappedServiceExecution of moveRoleplayCharacter function");
+        log.trace("WrappedServiceExecution of createRpCharMovement function");
         Movement movement = wrappedServiceExecution(dto, movementService::createRpCharMovement);
 
         log.info("Successfully handled request for creating rpchar movement!");
@@ -40,10 +42,21 @@ public class MovementRestController extends AbstractRestController {
     public HttpEntity<Movement> cancelRoleplayCharacterMove(@RequestBody DiscordIdDto dto) {
 
         log.debug("Incoming Post Request to create rp char movement, data [{}]", dto);
-        log.trace("WrappedServiceExecution of moveRoleplayCharacter function");
+        log.trace("WrappedServiceExecution of cancelRpCharMovement function");
         Movement movement = wrappedServiceExecution(dto, movementService::cancelRpCharMovement);
 
         log.info("Successfully handled request for creating rpchar movement!");
+        return ResponseEntity.ok(movement);
+    }
+
+    @PostMapping(PATH_MOVE_ARMY)
+    public HttpEntity<Movement> moveArmy(@RequestBody MoveArmyDto dto) {
+
+        log.debug("Incoming Post Request to create army movement, data [{}]", dto);
+        log.trace("WrappedServiceExecution of createArmyMovement function");
+        Movement movement = wrappedServiceExecution(dto, movementService::createArmyMovement);
+
+        log.info("Successfully handled request for creating army movement!");
         return ResponseEntity.ok(movement);
     }
 }
--- ALSpringBackend/src/test/java/com/ardaslegends/presentation/api/MovementRestControllerTest.java
+++ ALSpringBackend/src/test/java/com/ardaslegends/presentation/api/MovementRestControllerTest.java
@@ -3,6 +3,7 @@ package com.ardaslegends.presentation.api;
 import com.ardaslegends.data.domain.Movement;
 import com.ardaslegends.data.presentation.api.MovementRestController;
 import com.ardaslegends.data.service.MovementService;
+import com.ardaslegends.data.service.dto.army.MoveArmyDto;
 import com.ardaslegends.data.service.dto.player.DiscordIdDto;
 import com.ardaslegends.data.service.dto.player.rpchar.MoveRpCharDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -38,8 +39,8 @@ public class MovementRestControllerTest {
     }
 
     @Test
-    void ensureMoveRoleplayCharacterWorksCorrectly() throws Exception {
-        log.debug("Testing if moveRoleplayCharacter");
+    void ensureCreateRoleplayCharacterMovementWorksCorrectly() throws Exception {
+        log.debug("Testing if createRoleplayCharacterMovement");
 
         log.trace("Initializing Dto");
         MoveRpCharDto dto = new MoveRpCharDto("RandoId","12.S");
@@ -65,7 +66,7 @@ public class MovementRestControllerTest {
 
     @Test
     void ensureCancelRoleplayCharacterMoveWorksProperly() throws Exception {
-        log.debug("Testing if moveRoleplayCharacter");
+        log.debug("Testing if cancelRoleplayCharacterMovement works");
 
         log.trace("Initializing Dto");
         DiscordIdDto dto  = new DiscordIdDto("RandoId");
@@ -84,6 +85,32 @@ public class MovementRestControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders
                         .patch("http://localhost:8080/api/movement/cancel-char-move")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void ensureCreateArmyMovementWorksCorrectly() throws Exception {
+        log.debug("Testing if createArmyMovement works");
+
+        log.trace("Initializing Dto");
+        MoveArmyDto dto = new MoveArmyDto("1234","Knights of Gondor", "92");
+
+        log.trace("Initialize return movement");
+        Movement movement = Movement.builder().build();
+
+        log.trace("Initializing mock methods");
+        when(mockMovementService.createArmyMovement(dto)).thenReturn(movement);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+
+        String requestJson = ow.writeValueAsString(dto);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("http://localhost:8080/api/movement/move-army")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestJson))
                 .andExpect(status().isOk());
