70b0e9eccfae6b3a541e4be257795857da7d9dc1
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-08-09 18:49:39 +0200

    ArmyRepo::findAllByArmyType

--- ALSpringBackend/src/main/java/com/ardaslegends/data/repository/ArmyRepository.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/repository/ArmyRepository.java
@@ -1,17 +1,20 @@
 package com.ardaslegends.data.repository;
 
 import com.ardaslegends.data.domain.Army;
+import com.ardaslegends.data.domain.ArmyType;
 import com.ardaslegends.data.domain.ClaimBuild;
 import com.ardaslegends.data.service.ArmyService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ArmyRepository extends JpaRepository<Army, String> {
 
     public Optional<Army> findArmyByName(String name);
+    public List<Army> findAllByArmyType(ArmyType armyType);
 
     int countArmiesByOriginalClaimbuild(ClaimBuild claimBuild);
 }
