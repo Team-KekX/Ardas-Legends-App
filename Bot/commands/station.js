360331dc6b543fa46773cfef45f6d7b59a312cbd
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-08-18 13:27:34 +0200

    Removed unneccessary subcommands from station
    	- station character, armed company and trader
    	- all obsolete since station does it by itself

--- Bot/commands/station.js
+++ Bot/commands/station.js
@@ -7,32 +7,6 @@ module.exports = {
         .setDescription('Station a trader or armed company to take different actions.')
         .addSubcommand(subcommand =>
             subcommand
-                .setName('trader')
-                .setDescription('station a trader in a claimbuild')
-                .addStringOption(option =>
-                    option.setName('trader-name')
-                        .setDescription('The name of the trading company')
-                        .setRequired(true))
-                .addStringOption(option =>
-                    option.setName('claimbuild-name')
-                        .setDescription('The name of the claimbuild to station at')
-                        .setRequired(true))
-        )
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('armed-company')
-                .setDescription('Station an armed company in a claimbuild. Stations the army at the same time.')
-                .addStringOption(option =>
-                    option.setName('trader-name')
-                        .setDescription('The name of the armed company')
-                        .setRequired(true))
-                .addStringOption(option =>
-                    option.setName('claimbuild-name')
-                        .setDescription('The name of the claimbuild to station at')
-                        .setRequired(true))
-        )
-        .addSubcommand(subcommand =>
-            subcommand
                 .setName('army-or-company')
                 .setDescription('Station an army or company at a claimbuild.')
                 .addStringOption(option =>
@@ -43,20 +17,8 @@ module.exports = {
                     option.setName('claimbuild-name')
                         .setDescription('The name of the claimbuild to station at')
                         .setRequired(true))
-        )
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('character')
-                .setDescription('Station a character in a claimbuild.')
-                .addStringOption(option =>
-                    option.setName('character-name')
-                        .setDescription('The name of the roleplay character')
-                        .setRequired(true))
-                .addStringOption(option =>
-                    option.setName('claimbuild-name')
-                        .setDescription('The name of the claimbuild to station at')
-                        .setRequired(true))
         ),
+
     async execute(interaction) {
         // Dynamically get all subcommands for called command
         const commands = addSubcommands('station', false);
