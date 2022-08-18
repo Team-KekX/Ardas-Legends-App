05fd9eb85ea8da29620b1d1438b3a6d8be2e234c
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-08-18 14:51:50 +0200

    Heal-stop JS done

--- Bot/commands/subcommands/heal/heal_stop.js
+++ Bot/commands/subcommands/heal/heal_stop.js
@@ -1,16 +1,37 @@
 const {capitalizeFirstLetters} = require("../../../utils/utilities");
 const {MessageEmbed} = require('discord.js');
 const {HEAL} = require('../../../configs/embed_thumbnails.json');
+const {serverIP, serverPort} = require("../../../configs/config.json");
+const axios = require("axios");
 
 module.exports = {
     async execute(interaction) {
-        const name = capitalizeFirstLetters(interaction.options.getString('army-name').toLowerCase());
-        const replyEmbed = new MessageEmbed()
-            .setTitle(`Stop healing`)
-            .setColor('RED')
-            .setDescription(`${name} has stopped healing.`)
-            .setThumbnail(HEAL)
-            .setTimestamp()
-        await interaction.reply({embeds: [replyEmbed]});
+
+        const name = interaction.options.getString('army-name')
+
+        const data = {
+            executorDiscordId: interaction.member.id,
+            armyName: name
+        }
+
+        axios.patch("http://" + serverIP + ":" + serverPort + "/api/army/heal-stop", data)
+            .then(async function(response) {
+                const tokens = response.data.freeTokens;
+                const replyEmbed = new MessageEmbed()
+                    .setTitle(`Stop healing`)
+                    .setColor('GREEN')
+                    .setDescription(`${name} has stopped healing. Tokens: ${tokens}/30`)
+                    .setThumbnail(HEAL)
+                    .setTimestamp()
+                await interaction.reply({embeds: [replyEmbed]});
+            })
+            .catch(async function(error) {
+                const replyEmbed = new MessageEmbed()
+                    .setTitle("Error while stopping healing")
+                    .setColor("RED")
+                    .setDescription(error.response.data.message)
+                    .setTimestamp()
+                await interaction.reply({embeds: [replyEmbed]})
+            })
     },
 };
