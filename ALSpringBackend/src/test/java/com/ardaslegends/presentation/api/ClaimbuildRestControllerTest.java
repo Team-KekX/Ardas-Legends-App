b3ab5f8b86302ee45102f99ef72826000d60d7b4
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-08-27 00:55:46 +0200

    Add mockMvc Test for updateClaimbuildOwnee

--- ALSpringBackend/src/test/java/com/ardaslegends/presentation/api/ClaimbuildRestControllerTest.java
+++ ALSpringBackend/src/test/java/com/ardaslegends/presentation/api/ClaimbuildRestControllerTest.java
@@ -1,14 +1,24 @@
 package com.ardaslegends.presentation.api;
 
+import com.ardaslegends.data.domain.ClaimBuild;
+import com.ardaslegends.data.domain.Faction;
 import com.ardaslegends.data.presentation.api.ArmyRestController;
 import com.ardaslegends.data.presentation.api.ClaimbuildRestController;
 import com.ardaslegends.data.service.ArmyService;
 import com.ardaslegends.data.service.ClaimBuildService;
+import com.ardaslegends.data.service.dto.claimbuilds.UpdateClaimbuildOwnerDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.assertj.core.api.InstanceOfAssertFactories.map;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,7 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Slf4j
 public class ClaimbuildRestControllerTest {
     MockMvc mockMvc;
-    private ClaimBuildService mockClaimbuildService
+    private ClaimBuildService mockClaimbuildService;
     private ClaimbuildRestController claimbuildRestController;
 
     @BeforeEach
@@ -27,5 +37,42 @@ public class ClaimbuildRestControllerTest {
         mockClaimbuildService = mock(ClaimBuildService.class);
         claimbuildRestController = new ClaimbuildRestController(mockClaimbuildService);
         mockMvc = MockMvcBuilders.standaloneSetup(claimbuildRestController).build();
+    }
+
+    @Test
+    void ensureUpdateClaimbuildOwnerWorksProperly() throws Exception {
+        log.debug("Testing if updateClaimbuildOwner works properly with correct values");
+
+        UpdateClaimbuildOwnerDto dto = new UpdateClaimbuildOwnerDto("Claimbuild", "Gondor");
+
+        ClaimBuild claimBuild = ClaimBuild.builder()
+                .name(dto.claimbuildName())
+                .ownedBy(Faction.builder().name(dto.newFaction()).build())
+                .build();
+        when(mockClaimbuildService.setOwnerFaction(dto)).thenReturn(claimBuild);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+
+        String requestJson = ow.writeValueAsString(dto);
+
+        var result = mockMvc.perform((MockMvcRequestBuilders
+                        .patch("http://localhost:8080/api/claimbuild/update/claimbuild-faction")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var request = result.getRequest();
+        request.setCharacterEncoding("UTF-8");
+
+        UpdateClaimbuildOwnerDto response = mapper.readValue(request.getContentAsString()
+                ,UpdateClaimbuildOwnerDto.class);
+
+        assertThat(response.claimbuildName()).isEqualTo(claimBuild.getName());
+        assertThat(response.newFaction()).isEqualTo(claimBuild.getOwnedBy().getName());
+
+        log.info("Test passed: updateClaimbuildOwner builds the correct response");
     }
 }
