11a43257ea44420c2ad7520e77e46ec63b5989d6
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-08-26 23:57:48 +0200

    Added Base ClaimbuildRestController

---
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/presentation/api/ClaimbuildRestController.java
@@ -0,0 +1,16 @@
+package com.ardaslegends.data.presentation.api;
+
+import com.ardaslegends.data.presentation.AbstractRestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+
+@Slf4j
+@RestController
+@RequestMapping(ClaimbuildRestController.BASE_URL)
+public class ClaimbuildRestController extends AbstractRestController {
+    public static final String BASE_URL = "/api/army";
+}
