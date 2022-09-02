154ce53ccb5177f8541ea858be6742b3ca87252a
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-09-02 13:15:27 +0200

    Fixed updateRpCharTitle error replyEmbed

--- Bot/commands/subcommands/update/update_rpchar-title.js
+++ Bot/commands/subcommands/update/update_rpchar-title.js
@@ -27,8 +27,12 @@ module.exports = {
                 await interaction.reply({embeds: [replyEmbed]});
             })
             .catch(async function(error) {
-                //error occurred
-                await interaction.reply({content: `${error.response.data.message}`, ephemeral: false});
+                const replyEmbed = new MessageEmbed()
+                    .setTitle("Error while updating roleplay character title")
+                    .setColor("RED")
+                    .setDescription(error.response.data.message)
+                    .setTimestamp()
+                await interaction.reply({embeds: [replyEmbed]})
             })
 
     },
