e3ec7576edb73a15ef2d944ef344136681db5638
Luktronic <59698507+Luktronic@users.noreply.github.com>
2022-09-23 09:56:42 +0200

    Finished /update rpchar gear command
     Changes to be committed:
    	modified:   ALSpringBackend/src/main/java/com/ardaslegends/data/presentation/discord/commands/update/staff/UpdateRpcharGearCommand.java
    	modified:   docs/adoc/command-list/staff/roleplay-characters.adoc
    	modified:   docs/adoc/patch-notes/v1.x/v1.0.x/v1.0.2.adoc
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- ALSpringBackend/src/main/java/com/ardaslegends/data/presentation/discord/commands/update/staff/UpdateRpcharGearCommand.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/presentation/discord/commands/update/staff/UpdateRpcharGearCommand.java
@@ -1,14 +1,20 @@
 package com.ardaslegends.data.presentation.discord.commands.update.staff;
 
+import com.ardaslegends.data.domain.Player;
+import com.ardaslegends.data.domain.RPChar;
 import com.ardaslegends.data.presentation.discord.commands.ALCommandExecutor;
 import com.ardaslegends.data.presentation.discord.commands.ALStaffCommand;
 import com.ardaslegends.data.presentation.discord.config.BotProperties;
 import com.ardaslegends.data.presentation.discord.utils.ALColor;
 import com.ardaslegends.data.presentation.discord.utils.DiscordUtils;
 import com.ardaslegends.data.service.PlayerService;
+import com.ardaslegends.data.service.dto.player.UpdatePlayerIgnDto;
+import com.ardaslegends.data.service.dto.player.rpchar.UpdateRpCharDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.sql.Update;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.user.User;
 import org.javacord.api.interaction.SlashCommandInteraction;
 import org.javacord.api.interaction.SlashCommandInteractionOption;
 
@@ -23,8 +29,31 @@ public class UpdateRpcharGearCommand implements ALCommandExecutor, ALStaffComman
 
     @Override
     public EmbedBuilder execute(SlashCommandInteraction interaction, List<SlashCommandInteractionOption> options, BotProperties properties) {
+        log.debug("Executing /update rpchar gear request");
+
+        checkStaff(interaction, properties.getStaffRoles());
+
+        log.debug("Getting options");
+        User user = getUserOption("player", options);
+        log.debug("User: discord name [{}] - id [{}]", user.getName(), user.getIdAsString());
+
+        String gear = getStringOption("new-gear", options);
+        log.debug("New-Gear: [{}]", gear);
+
+        log.trace("Building dto");
+        UpdateRpCharDto dto = new UpdateRpCharDto(user.getIdAsString(), null, null, null, null, gear, null);
+        log.debug("Built dto with data [{}]", dto);
+
+        log.trace("Calling playerService");
+        RPChar rpChar = discordServiceExecution(dto, playerService::updateCharacterGear, "Error while updating Rp Char Gear");
+
+        log.debug("Building response Embed");
         return new EmbedBuilder()
-                .setTitle("Hello World")
+                .setTitle("Updated RpChar Gear")
+                .setDescription("Successfully changed gear of Roleplay Character %s".formatted(rpChar.getName()))
+                .addInlineField("Rp Char", rpChar.getName())
+                .addInlineField("New Gear", rpChar.getGear())
+                .addInlineField("User", user.getMentionTag())
                 .setColor(ALColor.YELLOW)
                 .setTimestampToNow();
     }
