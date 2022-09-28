afac20c787d0886df29b1d1dd6711da63b6b6051
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-09-28 19:47:54 +0200

    Added cancelHealArmy Command

--- ALSpringBackend/src/main/java/com/ardaslegends/data/presentation/discord/commands/cancel/CancelCommand.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/presentation/discord/commands/cancel/CancelCommand.java
@@ -3,6 +3,7 @@ package com.ardaslegends.data.presentation.discord.commands.cancel;
 import com.ardaslegends.data.presentation.discord.commands.ALCommand;
 import com.ardaslegends.data.presentation.discord.commands.ALCommandExecutor;
 import com.ardaslegends.data.presentation.discord.commands.move.MoveRpcharCommand;
+import com.ardaslegends.data.service.ArmyService;
 import com.ardaslegends.data.service.MovementService;
 import com.ardaslegends.data.service.PlayerService;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +23,7 @@ public class CancelCommand implements ALCommand {
 
     private final MovementService movementService;
     private final PlayerService playerService;
-
+    private final ArmyService armyService;
     @Override
     public SlashCommandBuilder init(Map<String, ALCommandExecutor> commands) {
         log.debug("Initializing /cancel command");
@@ -60,6 +61,19 @@ public class CancelCommand implements ALCommand {
                         .setOptions(Arrays.asList(
                                 new SlashCommandOptionBuilder()
                                         .setType(SlashCommandOptionType.SUB_COMMAND)
+                                        .setName("army")
+                                        .setDescription("Cancels the ongoing healing of an army")
+                                        .setOptions(Arrays.asList(
+                                                new SlashCommandOptionBuilder()
+                                                        .setType(SlashCommandOptionType.STRING)
+                                                        .setName("army")
+                                                        .setDescription("The name of the army")
+                                                        .setRequired(true)
+                                                        .build()
+                                        ))
+                                        .build(),
+                                new SlashCommandOptionBuilder()
+                                        .setType(SlashCommandOptionType.SUB_COMMAND)
                                         .setName("rpchar")
                                         .setDescription("Cancels the ongoing healing of your Roleplay Character")
                                         .build()
@@ -71,6 +85,8 @@ public class CancelCommand implements ALCommand {
         commands.put("cancel move rpchar", new CancelMoveRpcharCommand(movementService));
         commands.put("cancel heal rpchar", new CancelHealRpcharCommand(playerService));
         commands.put("cancel move army-or-company", new CancelMoveArmyOrCompany(movementService));
+
+        commands.put("cancel heal army", new CancelHealArmyCommand(armyService));
         log.info("Finished initializing /cancel command");
         return command;
     }
---
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/presentation/discord/commands/cancel/CancelHealArmyCommand.java
@@ -0,0 +1,44 @@
+package com.ardaslegends.data.presentation.discord.commands.cancel;
+
+import com.ardaslegends.data.presentation.discord.commands.ALCommandExecutor;
+import com.ardaslegends.data.presentation.discord.config.BotProperties;
+import com.ardaslegends.data.presentation.discord.utils.ALColor;
+import com.ardaslegends.data.presentation.discord.utils.Thumbnails;
+import com.ardaslegends.data.service.ArmyService;
+import com.ardaslegends.data.service.dto.army.UpdateArmyDto;
+import lombok.RequiredArgsConstructor;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.interaction.SlashCommandInteraction;
+import org.javacord.api.interaction.SlashCommandInteractionOption;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CancelHealArmyCommand implements ALCommandExecutor {
+
+    private final ArmyService armyService;
+
+    @Override
+    public EmbedBuilder execute(SlashCommandInteraction interaction, List<SlashCommandInteractionOption> options, BotProperties properties) {
+        log.debug("CancelHealArmy: Incoming /cancel heal army request");
+
+        String discordId = interaction.getUser().getIdAsString();
+
+        String armyName = getStringOption("army", options);
+        log.trace("CancelHealArmy: armyName is [{}]", armyName);
+
+        log.trace("CancelHealArmy: Building UpdateArmyDto");
+        UpdateArmyDto dto = new UpdateArmyDto(discordId, armyName, null, null);
+
+        log.debug("CancelHealArmy: Calling armyService cancelArmy");
+        var army = discordServiceExecution(dto, armyService::healStop, "Error while stopping heal");
+
+        log.debug("CancelHealArmy: Building EmbedBuilder");
+        return new EmbedBuilder()
+                .setTitle("Cancelled healing of army")
+                .setDescription("The healing of army '%s' has been stopped \nSome units may not be healed!".formatted(army.getName()))
+                .setThumbnail(getFactionBanner(army.getFaction().getName()))
+                .setTimestampToNow()
+                .setColor(ALColor.GREEN);
+    }
+}
