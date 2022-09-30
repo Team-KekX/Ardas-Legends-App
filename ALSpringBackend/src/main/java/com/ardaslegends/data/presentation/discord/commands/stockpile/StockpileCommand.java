d9933173aa41cf4a0a380a10a606bbb9a59a3299
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-09-30 10:59:01 +0200

    Added stockpileRemove Comamnd

--- ALSpringBackend/src/main/java/com/ardaslegends/data/presentation/discord/commands/stockpile/StockpileCommand.java
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/presentation/discord/commands/stockpile/StockpileCommand.java
@@ -3,6 +3,7 @@ package com.ardaslegends.data.presentation.discord.commands.stockpile;
 import com.ardaslegends.data.presentation.discord.commands.ALCommand;
 import com.ardaslegends.data.presentation.discord.commands.ALCommandExecutor;
 import com.ardaslegends.data.presentation.discord.commands.stockpile.staff.StockpileAddCommand;
+import com.ardaslegends.data.presentation.discord.commands.stockpile.staff.StockpileRemoveCommand;
 import com.ardaslegends.data.service.FactionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -47,10 +48,30 @@ public class StockpileCommand implements ALCommand {
                                         .setRequired(true)
                                         .build()
                         ))
+                        .build(),
+                new SlashCommandOptionBuilder()
+                        .setType(SlashCommandOptionType.SUB_COMMAND)
+                        .setName("remove")
+                        .setDescription("Removes an amount to the factions stockpile")
+                        .setOptions(Arrays.asList(
+                                new SlashCommandOptionBuilder()
+                                        .setType(SlashCommandOptionType.STRING)
+                                        .setName("faction")
+                                        .setRequired(true)
+                                        .setDescription("The faction whose stockpile gets changed")
+                                        .build(),
+                                new SlashCommandOptionBuilder()
+                                        .setType(SlashCommandOptionType.LONG)
+                                        .setName("amount")
+                                        .setDescription("The amount that gets removed from the stockpile")
+                                        .setRequired(true)
+                                        .build()
+                        ))
                         .build()
         ));
 
         commands.put("stockpile add", new StockpileAddCommand(factionService));
+        commands.put("stockpile remove", new StockpileRemoveCommand(factionService));
 
         log.info("Finished initializing /stockpile commands");
         return command;
---
+++ ALSpringBackend/src/main/java/com/ardaslegends/data/presentation/discord/commands/stockpile/staff/StockpileRemoveCommand.java
@@ -0,0 +1,51 @@
+package com.ardaslegends.data.presentation.discord.commands.stockpile.staff;
+
+import com.ardaslegends.data.presentation.discord.commands.ALStaffCommandExecutor;
+import com.ardaslegends.data.presentation.discord.config.BotProperties;
+import com.ardaslegends.data.presentation.discord.utils.ALColor;
+import com.ardaslegends.data.service.FactionService;
+import com.ardaslegends.data.service.dto.faction.UpdateStockpileDto;
+import lombok.RequiredArgsConstructor;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.interaction.SlashCommandInteraction;
+import org.javacord.api.interaction.SlashCommandInteractionOption;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class StockpileRemoveCommand implements ALStaffCommandExecutor {
+
+    private final FactionService factionService;
+
+    @Override
+    public EmbedBuilder execute(SlashCommandInteraction interaction, List<SlashCommandInteractionOption> options, BotProperties properties) {
+        log.debug("Incoming /stockpile remove request");
+
+        checkStaff(interaction, properties.getStaffRoles());
+        log.trace("StockpileRemove: User is staff -> allowed");
+
+
+        var factionName = getStringOption("faction", options);
+        log.trace("StockpileRemove: faction name is [{}]", factionName);
+
+        var amount = getLongOption("amount", options).intValue();
+        log.trace("StockpileRemove: amount is [{}]", amount);
+
+        log.trace("StockpileRemove: Building Dto");
+        UpdateStockpileDto dto = new UpdateStockpileDto(factionName, amount);
+
+        log.debug("StockpileRemove: Calling discordService");
+        var result = discordServiceExecution(dto, factionService::removeFromStockpile, "Error while updating stockpile");
+
+        log.debug("StockpileRemove: Building Embed");
+        return new EmbedBuilder()
+                .setTitle("Removed from food stockpile")
+                .setThumbnail(getFactionBanner(factionName))
+                .setTimestampToNow()
+                .setDescription("Removed %s stacks of food to %s's stockpile".formatted(amount, factionName))
+                .addInlineField("Faction", result.getName())
+                .addInlineField("Stockpile in Stacks", result.getFoodStockpile().toString())
+                .setColor(ALColor.YELLOW);
+    }
+
+}
