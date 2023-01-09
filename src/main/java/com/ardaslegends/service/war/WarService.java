822111a24df53867de5737854fe2f8e91f5af0f8
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-01-09 22:43:52 +0100

    Started createWar (War Declaration) service method

--- src/main/java/com/ardaslegends/service/war/WarService.java
+++ src/main/java/com/ardaslegends/service/war/WarService.java
@@ -1,5 +1,6 @@
 package com.ardaslegends.service.war;
 
+import com.ardaslegends.domain.Faction;
 import com.ardaslegends.domain.war.War;
 import com.ardaslegends.repository.WarRepository;
 import com.ardaslegends.service.AbstractService;
@@ -9,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
+
 
 @RequiredArgsConstructor
 @Slf4j
@@ -17,5 +20,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class WarService extends AbstractService<War, WarRepository> {
 
     private final FactionService factionService;
+
+    @Transactional(readOnly = false)
+    public War createWar(Faction attackerName, Faction defenderName, String executorDiscordId) {
+        log.debug("Creating war with data [aggressor: {}, defender: {}]", attackerName, defenderName);
+
+        Objects.requireNonNull(attackerName, "Attacker Faction name must not be null");
+        Objects.requireNonNull(defenderName, "Defender Faction name must not be null");
+        Objects.requireNonNull(executorDiscordId, "Discord ID of user who executed the command must not be null");
+
+
+        return null;
+    }
 
 }
