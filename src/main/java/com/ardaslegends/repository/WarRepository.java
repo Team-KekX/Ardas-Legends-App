547b6755c832fdb36ab0e5c34e27de1c9bd1bfe7
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-01-23 09:34:45 +0100

    Added findAllWarsWithAggressor Query

--- src/main/java/com/ardaslegends/repository/WarRepository.java
+++ src/main/java/com/ardaslegends/repository/WarRepository.java
@@ -1,12 +1,22 @@
 package com.ardaslegends.repository;
 
+import com.ardaslegends.domain.Faction;
 import com.ardaslegends.domain.war.War;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface WarRepository extends JpaRepository<War, Long> {
     public Optional<War> findByName(String name);
+
+    @Query("""
+        FROM War 
+        where War.aggressors.warParticipant = :aggressor 
+        """)
+    public List<War> findAllWarsWithAggressor(@Param("aggressor") Faction aggressor);
 }
