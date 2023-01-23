7ae02ca5185e3fdd28506296f82712f38c0e60e2
Luktronic <59698507+Luktronic@users.noreply.github.com>
2023-01-23 14:20:37 +0100

    Moved CreateWarDto to presentation layer
     Changes to be committed:
    	renamed:    src/main/java/com/ardaslegends/service/dto/war/CreateWarResponse.java -> src/main/java/com/ardaslegends/presentation/api/response/war/CreateWarResponse.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/presentation/api/response/war/CreateWarResponse.java
+++ src/main/java/com/ardaslegends/presentation/api/response/war/CreateWarResponse.java
@@ -1,4 +1,4 @@
-package com.ardaslegends.service.dto.war;
+package com.ardaslegends.presentation.api.response.war;
 
 import com.ardaslegends.domain.Faction;
 import org.javacord.api.entity.permission.Role;
