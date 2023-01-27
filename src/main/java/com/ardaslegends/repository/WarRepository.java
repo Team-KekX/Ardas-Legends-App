edad8303fdb0c7a7c5bd6bf3216184c92a3cfbb0
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-01-27 10:58:38 +0100

    Ree forgot to remove import statements

--- src/main/java/com/ardaslegends/repository/WarRepository.java
+++ src/main/java/com/ardaslegends/repository/WarRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
