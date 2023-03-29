03ee6e1200701ecc5d95200a4d06ed4af753a704
Luktronic <59698507+Luktronic@users.noreply.github.com>
2023-03-29 21:24:17 +0200

    Added OpenAPI spec for UnitResponse
     Changes to be committed:
    	modified:   src/main/java/com/ardaslegends/presentation/api/response/army/unit/UnitResponse.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/presentation/api/response/army/unit/UnitResponse.java
+++ src/main/java/com/ardaslegends/presentation/api/response/army/unit/UnitResponse.java
@@ -1,13 +1,27 @@
 package com.ardaslegends.presentation.api.response.army.unit;
 
 import com.ardaslegends.domain.Unit;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@Schema(name = "UnitResponse", description = "Unit response containing all information")
 public record UnitResponse(
+        @Schema(description = "Name of the Unit", example = "Gondor Archer")
         String name,
+        @Schema(description = "The base cost of the unit (how many tokens one of that unit costs)",
+                example = "1.5")
         Double baseCost,
+        @Schema(description = "If the unit is mounted")
         Boolean isMounted,
+        @Schema(description = "How many of that Unit is in the army. This number is the" +
+                "'maximum' amount of that unit in the army.", example = "10")
         Integer count,
+        @Schema(description = "How many of that unit are alive. I.e. if the 'count' is 10, then a max. of 10 units can be alive",
+                example = "5", maximum = "same number as 'count'", minimum = "0")
         Integer alive,
+        @Schema(description = "The total amount of tokens that this unit occupies. TotalCost = baseCost * count",
+            example = "15", minimum = "baseCost * count")
         Double totalCost
 
 
