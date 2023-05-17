c6772c9c38b5fd6c3598e8b91c5caa3e762fad79
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-05-17 23:04:23 +0200

    Working on safe findByDiscordId Implementation

--- src/main/java/com/ardaslegends/repository/player/PlayerRepositoryImpl.java
+++ src/main/java/com/ardaslegends/repository/player/PlayerRepositoryImpl.java
@@ -1,8 +1,11 @@
 package com.ardaslegends.repository.player;
 
 import com.ardaslegends.domain.Player;
+import lombok.val;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class PlayerRepositoryImpl extends QuerydslRepositorySupport implements PlayerRepositoryCustom {
@@ -12,6 +15,7 @@ public class PlayerRepositoryImpl extends QuerydslRepositorySupport implements P
 
     @Override
     public Player queryByDiscordId(String discordId) {
+        Objects.requireNonNull(discordId);
 
         return null;
     }
