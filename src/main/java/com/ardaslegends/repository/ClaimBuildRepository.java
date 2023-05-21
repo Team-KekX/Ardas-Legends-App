2f5cb3c71080d8528aef5949b5b6633255eb865f
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-05-21 22:00:23 +0200

    CbRepo now extends CbRepoCustom

--- src/main/java/com/ardaslegends/repository/ClaimBuildRepository.java
+++ src/main/java/com/ardaslegends/repository/ClaimBuildRepository.java
@@ -1,13 +1,14 @@
 package com.ardaslegends.repository;
 
 import com.ardaslegends.domain.ClaimBuild;
+import com.ardaslegends.repository.claimbuild.ClaimbuildRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ClaimBuildRepository extends JpaRepository<ClaimBuild, Long> {
+public interface ClaimBuildRepository extends JpaRepository<ClaimBuild, Long>, ClaimbuildRepositoryCustom {
 
     Optional<ClaimBuild> findClaimBuildByName(String name);
 }
