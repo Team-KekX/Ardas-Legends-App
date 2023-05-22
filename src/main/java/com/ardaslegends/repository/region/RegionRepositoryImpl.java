7f9e0639deb8c634484b8aaca896d21b8aa01cde
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-05-22 08:10:54 +0200

    Added RegionRepo QueryDSL Support

--- src/main/java/com/ardaslegends/repository/region/RegionRepositoryImpl.java
+++ src/main/java/com/ardaslegends/repository/region/RegionRepositoryImpl.java
@@ -1,10 +1,40 @@
 package com.ardaslegends.repository.region;
 
+import com.ardaslegends.domain.QRegion;
 import com.ardaslegends.domain.Region;
+import com.ardaslegends.repository.exceptions.RegionRepositoryException;
+import lombok.val;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.Objects;
+import java.util.Optional;
 
 public class RegionRepositoryImpl extends QuerydslRepositorySupport implements RegionRepositoryCustom {
     public RegionRepositoryImpl() {
         super(Region.class);
+    }
+
+    @Override
+    public Region queryById(String id) {
+        val fetchedRegion = queryByIdOptional(id);
+
+        if(fetchedRegion.isEmpty()) {
+            throw RegionRepositoryException.entityNotFound("id", id);
+        }
+
+        return fetchedRegion.get();
+    }
+
+    @Override
+    public Optional<Region> queryByIdOptional(String id) {
+        Objects.requireNonNull(id);
+
+        QRegion qRegion = QRegion.region;
+
+        val fetchedRegion = from(qRegion)
+                .where(qRegion.id.equalsIgnoreCase(id))
+                .fetchFirst();
+
+        return Optional.of(fetchedRegion);
     }
 }
