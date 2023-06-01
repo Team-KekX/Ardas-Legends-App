d19505d5e11de45f9e0b576c1ce6a8f6dd559a17
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-06-01 21:14:17 +0200

    Added ProductionSiteTypeEnumConverter

--- src/main/java/com/ardaslegends/configuration/WebConfig.java
+++ src/main/java/com/ardaslegends/configuration/WebConfig.java
@@ -1,6 +1,7 @@
 package com.ardaslegends.configuration;
 
 import com.ardaslegends.configuration.converter.ClaimbuildTypeEnumConverter;
+import com.ardaslegends.configuration.converter.ProductionSiteTypeEnumConverter;
 import com.ardaslegends.configuration.converter.RegionTypeEnumConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -12,5 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new RegionTypeEnumConverter());
         registry.addConverter(new ClaimbuildTypeEnumConverter());
+        registry.addConverter(new ProductionSiteTypeEnumConverter());
     }
 }
