d460bc340b2cc78780affd195410bdb3c602008e
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-06-01 19:41:21 +0200

    Added fetchAll to RegionRepo

--- src/main/java/com/ardaslegends/repository/region/RegionRepositoryCustom.java
+++ src/main/java/com/ardaslegends/repository/region/RegionRepositoryCustom.java
@@ -3,9 +3,11 @@ package com.ardaslegends.repository.region;
 import com.ardaslegends.domain.Region;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface RegionRepositoryCustom {
 
+    Set<Region> queryAll();
     Region queryById(String id);
     Optional<Region> queryByIdOptional(String id);
 
