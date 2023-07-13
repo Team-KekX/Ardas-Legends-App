cab770ba7e498d1d272cb2dd5a248875ebce6b51
Luktronic <luka.furundzija@gmx.at>
2023-07-13 22:38:06 +0200

    Added rpchars in region to RegionResponse and GetRegion endpoint
     Changes to be committed:
    	modified:   src/main/java/com/ardaslegends/presentation/api/RegionController.java
    	modified:   src/main/java/com/ardaslegends/presentation/api/response/region/RegionResponseDetailed.java
    	modified:   src/main/java/com/ardaslegends/service/RegionService.java
    	new file:   src/main/java/com/ardaslegends/service/exceptions/region/RegionServiceException.java
    	modified:   src/test/java/com/ardaslegends/presentation/api/ClaimbuildRestControllerTest.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/presentation/api/RpCharRestController.java
+++ src/main/java/com/ardaslegends/presentation/api/RpCharRestController.java
@@ -1,39 +1,16 @@
 package com.ardaslegends.presentation.api;
 
-import com.ardaslegends.presentation.AbstractRestController;
-import com.ardaslegends.presentation.api.response.player.rpchar.RpCharOwnerResponse;
-import com.ardaslegends.service.RpCharService;
-import lombok.RequiredArgsConstructor;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RequiredArgsConstructor
-
 @RestController
-@RequestMapping(RpCharRestController.BASE_URL)
-public class RpCharRestController extends AbstractRestController {
+@Tag(name = "RPChar Controller", description = "All REST endpoints regarding Roleplay Characters")
+@RequestMapping(RPCharRestController.BASE_URL)
+public class RPCharRestController {
 
-    public static final String BASE_URL = "/api/rpchars";
-    private static final String GET_ALL = "/all";
-
-    private final RpCharService rpCharService;
-
-    @GetMapping(GET_ALL)
-    public ResponseEntity<Slice<RpCharOwnerResponse>> getAll(Pageable pageable) {
-        log.info("Incoming rpchars getAllSliced request with pageable [{}]", pageable);
-
-        val slicedRpchars = wrappedServiceExecution(pageable, rpCharService::getAll);
-        val slicedRpCharOwnerResponse = slicedRpchars.map(RpCharOwnerResponse::new);
-
-        return ResponseEntity.ok(slicedRpCharOwnerResponse);
-    }
+    public final static String BASE_URL = "/api/rpchars";
 
 }
