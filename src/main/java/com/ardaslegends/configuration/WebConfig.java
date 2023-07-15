e7d0b4d296352acbd9a69e5443ee4a4ccfae82a6
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-07-16 00:42:52 +0200

    Added ResourceTypeEnumConverter

--- src/main/java/com/ardaslegends/configuration/WebConfig.java
+++ src/main/java/com/ardaslegends/configuration/WebConfig.java
@@ -1,9 +1,6 @@
 package com.ardaslegends.configuration;
 
-import com.ardaslegends.configuration.converter.ClaimbuildTypeEnumConverter;
-import com.ardaslegends.configuration.converter.ProductionSiteTypeEnumConverter;
-import com.ardaslegends.configuration.converter.RegionTypeEnumConverter;
-import com.ardaslegends.configuration.converter.SpecialBuildingEnumConverter;
+import com.ardaslegends.configuration.converter.*;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -19,6 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addConverter(new ClaimbuildTypeEnumConverter());
         registry.addConverter(new ProductionSiteTypeEnumConverter());
         registry.addConverter(new SpecialBuildingEnumConverter());
+        registry.addConverter(new ResourceTypeEnumConverter());
     }
 
     @Override
