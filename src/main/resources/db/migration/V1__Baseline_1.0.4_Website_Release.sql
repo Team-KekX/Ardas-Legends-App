1d2b27d77989f056626f16cb87830681f53269f3
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-08-16 20:44:14 +0200

    Remove creation of flyway_history table from baseline migration

--- src/main/resources/db/migration/V1__Baseline_1.0.4_Website_Release.sql
+++ src/main/resources/db/migration/V1__Baseline_1.0.4_Website_Release.sql
@@ -1,18 +1,3 @@
-CREATE TABLE flyway_schema_history
-(
-    installed_rank INTEGER                                   NOT NULL,
-    version        VARCHAR(50),
-    description    VARCHAR(200)                              NOT NULL,
-    type           VARCHAR(20)                               NOT NULL,
-    script         VARCHAR(1000)                             NOT NULL,
-    checksum       INTEGER,
-    installed_by   VARCHAR(100)                              NOT NULL,
-    installed_on   TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-    execution_time INTEGER                                   NOT NULL,
-    success        BOOLEAN                                   NOT NULL,
-    CONSTRAINT flyway_schema_history_pk PRIMARY KEY (installed_rank)
-);
-
 CREATE TABLE roleplay_apps_accepted_by
 (
     roleplay_application_id BIGINT NOT NULL,
