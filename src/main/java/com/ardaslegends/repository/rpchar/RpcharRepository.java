46108c4b34bc486681db5d4c7a4f8aded3f03378
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-01-18 20:12:37 +0100

    Add findRpcharByName repository method

--- src/main/java/com/ardaslegends/repository/rpchar/RpcharRepository.java
+++ src/main/java/com/ardaslegends/repository/rpchar/RpcharRepository.java
@@ -4,6 +4,9 @@ import com.ardaslegends.domain.RPChar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RpcharRepository extends JpaRepository<RPChar, Long>, RpcharRepositoryCustom {
+    Optional<RPChar> findRpcharByName(String name);
 }
