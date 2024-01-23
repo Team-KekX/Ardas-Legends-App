238eccbd0a4791efe161c7818f8bfc1cf707b6b3
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-01-23 20:21:31 +0100

    Add SecurityConfig for rest endpoints

---
+++ src/main/java/com/ardaslegends/configuration/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.ardaslegends.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests ->
+                        requests
+                                //.requestMatchers("/auth/authorize").permitAll()
+                                .anyRequest().permitAll()
+                );
+
+
+        return http.build();
+    }
+
+}
