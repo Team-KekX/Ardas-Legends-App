3ad27da544faf871c4177261545096dd373b3336
Luktronic <luka.furundzija@gmx.at>
2024-01-24 19:36:35 +0100

    Added DiscordService/sendMessageToRpChannel
     Changes to be committed:
    	modified:   src/main/java/com/ardaslegends/service/discord/DiscordService.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/service/discord/DiscordService.java
+++ src/main/java/com/ardaslegends/service/discord/DiscordService.java
@@ -1,13 +1,18 @@
 package com.ardaslegends.service.discord;
 
+import com.ardaslegends.presentation.discord.commands.ALMessageResponse;
 import com.ardaslegends.presentation.discord.config.BotProperties;
+import com.ardaslegends.service.discord.messages.ALMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.permission.Role;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -28,5 +33,29 @@ public class DiscordService {
         else
             log.debug("No role with id [{}] found", roleId);
         return foundRole;
+    }
+
+    public Message sendMessageToRpChannel(ALMessage message) {
+        log.debug("Trying to send message [{}] to Roleplay Channel", message);
+        return sendMessage(message, botProperties.getGeneralRpCommandsChannel());
+    }
+
+    private Message sendMessage(ALMessage message, TextChannel channel) {
+        log.debug("Trying to send message [{}] to channel [{}]", message, channel.getIdAsString());
+
+        Objects.requireNonNull(message, "Message was null!");
+        Objects.requireNonNull(channel, "TextChannel was null!");
+
+        Message returnMessage = null;
+        if(message.hasMessage()) {
+            log.debug("Message has a message [{}]", message.message());
+            returnMessage = message.message().send(channel).join();
+        }
+        else {
+            log.debug("Message only consists of {} embeds [{}]", message.embeds().size(), message.embeds());
+            returnMessage = channel.sendMessage(message.embeds()).join();
+        }
+
+        return returnMessage;
     }
 }
