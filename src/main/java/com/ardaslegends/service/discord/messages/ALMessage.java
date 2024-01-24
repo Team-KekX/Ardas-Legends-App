c31ddabf56e961a9a8fe1109da3364a31d6f3a0d
Luktronic <luka.furundzija@gmx.at>
2024-01-24 19:24:40 +0100

    Refactored ALMessage to allow multiple embeds
     Changes to be committed:
    	modified:   src/main/java/com/ardaslegends/service/discord/messages/ALMessage.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/service/discord/messages/ALMessage.java
+++ src/main/java/com/ardaslegends/service/discord/messages/ALMessage.java
@@ -4,10 +4,24 @@ import jakarta.validation.constraints.NotNull;
 import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 
-public record ALMessage (MessageBuilder message, @NotNull EmbedBuilder embed) {
+import java.util.List;
+
+public record ALMessage (MessageBuilder message, @NotNull List<EmbedBuilder> embeds) {
+
+    public ALMessage {
+        if(message != null)
+            message.addEmbeds(embeds);
+    }
 
     public boolean hasMessage() {
         return message != null;
     }
 
+    @Override
+    public String toString() {
+        return "ALMessage{" +
+                "message=" + message +
+                ", embeds=" + embeds +
+                '}';
+    }
 }
