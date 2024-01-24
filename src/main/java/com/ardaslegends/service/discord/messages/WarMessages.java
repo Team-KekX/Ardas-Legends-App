7c51c0b11cf8c49dbe1b01b9af34b810548f3494
Luktronic <luka.furundzija@gmx.at>
2024-01-24 18:49:44 +0100

    Moved WarMessages to war subpackage
     Changes to be committed:
    	deleted:    src/main/java/com/ardaslegends/service/discord/messages/WarMessages.java
    	new file:   src/main/java/com/ardaslegends/service/discord/messages/war/WarMessages.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/service/discord/messages/WarMessages.java
+++
@@ -1,4 +0,0 @@
-package com.ardaslegends.service.discord.messages;
-
-public class WarMessages {
-}
---
+++ src/main/java/com/ardaslegends/service/discord/messages/war/WarMessages.java
@@ -0,0 +1,4 @@
+package com.ardaslegends.service.discord.messages.war;
+
+public class WarMessages {
+}
