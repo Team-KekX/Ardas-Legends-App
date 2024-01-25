2d9eae871c6be2fd6a3935b5b53e732be24de0de
Luktronic <luka.furundzija@gmx.at>
2024-01-25 18:53:16 +0100

    Added NotFoundException/genericNotFound
     Changes to be committed:
    	modified:   src/main/java/com/ardaslegends/repository/exceptions/NotFoundException.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/repository/exceptions/NotFoundException.java
+++ src/main/java/com/ardaslegends/repository/exceptions/NotFoundException.java
@@ -3,7 +3,7 @@ package com.ardaslegends.repository.exceptions;
 public class NotFoundException extends DataAccessException{
 
 
-    private final String GENERIC_COULD_NOT_FIND = "Could not find %s with %s %s!";
+    private static final String GENERIC_COULD_NOT_FIND = "Could not find %s with %s %s!";
 
     protected NotFoundException(String message) {
         super(message);
@@ -11,6 +11,13 @@ public class NotFoundException extends DataAccessException{
 
     protected NotFoundException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    /**
+     * Results in an error with format "Could not find %s with %s %s!"
+     */
+    public static NotFoundException genericNotFound(String couldNotFind, String with, String value) {
+        return new NotFoundException(GENERIC_COULD_NOT_FIND.formatted(couldNotFind, with, value));
     }
 
 }
