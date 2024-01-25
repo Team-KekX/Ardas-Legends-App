b82fb22877305bfcb880ec200b776abed716c8d4
Luktronic <luka.furundzija@gmx.at>
2024-01-25 19:57:40 +0100

    Added WarServiceException/warNotActive
     Changes to be committed:
    	modified:   src/main/java/com/ardaslegends/service/exceptions/logic/war/WarServiceException.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/service/exceptions/logic/war/WarServiceException.java
+++ src/main/java/com/ardaslegends/service/exceptions/logic/war/WarServiceException.java
@@ -1,6 +1,7 @@
 package com.ardaslegends.service.exceptions.logic.war;
 
 import com.ardaslegends.service.exceptions.logic.LogicException;
+import com.ardaslegends.service.war.WarService;
 
 public class WarServiceException extends LogicException {
 
@@ -11,6 +12,7 @@ public class WarServiceException extends LogicException {
     private static final String CANNOT_DECLARE_WAR_ON_YOUR_FACTION = "You cannot declare war on your own faction!";
     private static final String ALREADY_AT_WAR = "Your faction '%s' is already at war with '%s'!";
     private static final String NO_WAR_WITH_NAME = "No war with name '%s' found!";
+    private static final String WAR_NOT_ACTIVE = "The war '%s' is already over!";
 
     public static WarServiceException noWarDeclarationPermissions() { return new WarServiceException(NO_WAR_DECLARATION_PERMISSIONS); }
     public static WarServiceException factionAlreadyJoinedTheWarAsAttacker(String factionName) { return new WarServiceException(FACTION_ALREADY_JOINED_THE_WAR_AS_ATTACKER.formatted(factionName));}
@@ -19,6 +21,7 @@ public class WarServiceException extends LogicException {
     public static WarServiceException cannotDeclareWarOnYourFaction() { return new WarServiceException(CANNOT_DECLARE_WAR_ON_YOUR_FACTION); }
     public static WarServiceException alreadyAtWar(String executorFaction, String otherFaction) { return new WarServiceException(ALREADY_AT_WAR.formatted(executorFaction, otherFaction)); }
     public static WarServiceException noWarWithNameFound(String name) {return new WarServiceException(NO_WAR_WITH_NAME.formatted(name));}
+    public static WarServiceException warNotActive(String name) {return new WarServiceException(WAR_NOT_ACTIVE.formatted(name));}
 
     public WarServiceException(String message, Throwable rootCause) {
         super(message, rootCause);
