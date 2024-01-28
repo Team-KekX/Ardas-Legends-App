c816f1fe376f77c8900e9595065b3fb59552a383
Luktronic <luka.furundzija@gmx.at>
2024-01-29 00:35:48 +0100

    Added more information to WarResponse
     Changes to be committed:
    	modified:   src/main/java/com/ardaslegends/presentation/api/response/war/WarResponse.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/presentation/api/response/war/WarResponse.java
+++ src/main/java/com/ardaslegends/presentation/api/response/war/WarResponse.java
@@ -1,15 +1,21 @@
 package com.ardaslegends.presentation.api.response.war;
 
+import com.ardaslegends.domain.Faction;
 import com.ardaslegends.domain.war.War;
+import com.ardaslegends.domain.war.WarParticipant;
 
 import java.time.OffsetDateTime;
 
 public record WarResponse(
         long id,
-        String nameOfWar,
-        String nameOfAttacker,
-        String nameOfDefender,
-        OffsetDateTime startDate
+        String name,
+        String initialAttacker,
+        String initialDefender,
+        String[] agressors,
+        String[] defenders,
+        OffsetDateTime startDate,
+        OffsetDateTime endDate,
+        boolean isActive
 ) {
     public WarResponse(War war) {
         this(
@@ -17,7 +23,17 @@ public record WarResponse(
                 war.getName(),
                 war.getInitialAttacker().getWarParticipant().getName(),
                 war.getInitialDefender().getWarParticipant().getName(),
-                war.getStartDate()
+                war.getAggressors().stream()
+                        .map(WarParticipant::getWarParticipant)
+                        .map(Faction::getName)
+                        .toArray(String[]::new),
+                war.getDefenders().stream()
+                        .map(WarParticipant::getWarParticipant)
+                        .map(Faction::getName)
+                        .toArray(String[]::new),
+                war.getStartDate(),
+                war.getEndDate(),
+                war.getIsActive()
         );
     }
 }
