3c51f2a3f9846b755885aa4db9f39d9f1d624aab
Luktronic <luka.furundzija@gmx.at>
2024-01-29 00:30:07 +0100

    Renamed PaginatedWarsResponse to WarResponse
     Changes to be committed:
    	modified:   src/main/java/com/ardaslegends/presentation/api/WarRestController.java
    	renamed:    src/main/java/com/ardaslegends/presentation/api/response/war/PaginatedWarsResponse.java -> src/main/java/com/ardaslegends/presentation/api/response/war/WarResponse.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/presentation/api/response/war/WarResponse.java
+++ src/main/java/com/ardaslegends/presentation/api/response/war/WarResponse.java
@@ -2,18 +2,16 @@ package com.ardaslegends.presentation.api.response.war;
 
 import com.ardaslegends.domain.war.War;
 
-import jakarta.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
-import java.util.Objects;
 
-public record PaginatedWarsResponse(
+public record WarResponse(
         long id,
         String nameOfWar,
         String nameOfAttacker,
         String nameOfDefender,
         OffsetDateTime startDate
 ) {
-    public PaginatedWarsResponse(War war) {
+    public WarResponse(War war) {
         this(
                 war.getId(),
                 war.getName(),
