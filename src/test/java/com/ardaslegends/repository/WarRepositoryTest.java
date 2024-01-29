5552347651483165282df081995a384a3b3e78bf
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-01-29 18:29:53 +0100

    Add WarRepository Test class with setup()

---
+++ src/test/java/com/ardaslegends/repository/WarRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.ardaslegends.repository;
+
+import com.ardaslegends.domain.Faction;
+import com.ardaslegends.domain.Region;
+import com.ardaslegends.domain.RegionType;
+import com.ardaslegends.domain.war.War;
+import com.ardaslegends.domain.war.WarParticipant;
+import com.ardaslegends.repository.war.WarRepository;
+import io.vavr.collection.List;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest(properties = {"spring.sql.init.mode=never"})
+@ActiveProfiles("test")
+public class WarRepositoryTest {
+
+    @Autowired
+    WarRepository warRepository;
+
+    @BeforeEach
+    void setup() {
+        val region = Region.builder().name("").id("1").regionType(RegionType.DESERT).build();
+        val region2 = Region.builder().name("").id("2").regionType(RegionType.DESERT).build();
+        val region3 = Region.builder().name("").id("3").regionType(RegionType.DESERT).build();
+        val region4 = Region.builder().name("").id("4").regionType(RegionType.DESERT).build();
+        val region5 = Region.builder().name("").id("5").regionType(RegionType.DESERT).build();
+
+        var gondor = Faction.builder().name("Gondor").homeRegion(region).build();
+        var mordor = Faction.builder().name("Mordor").homeRegion(region2).build();
+        var arnor = Faction.builder().name("Arnor").homeRegion(region3).build();
+        var rivendell = Faction.builder().name("Rivendell").homeRegion(region4).build();
+        var umbar = Faction.builder().name("Umbar").homeRegion(region5).build();
+
+        /*
+        Gondor = 3 (2 active, 1 inactive)
+         */
+
+        War war1 = new War("Minas Ithil", gondor, mordor);
+        war1.addToAggressors(arnor);
+        war1.addToDefenders(umbar);
+
+        var war2 = new War("Something else", mordor, rivendell);
+        war2.addToDefenders(arnor);
+        war2.addToAggressors(umbar);
+
+        war2.end();
+
+        var war3 = new War("Another thing", mordor, arnor);
+        war3.addToDefenders(gondor);
+
+        var war4 = new War("Keke", mordor, gondor);
+        war4.end();
+
+        warRepository.saveAll(List.of(war1, war2, war3, war4));
+    }
+}
