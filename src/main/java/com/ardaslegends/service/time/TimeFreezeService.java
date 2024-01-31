526fac5ce69d0e15cc068d2abd86a2ce8e05cba2
Luktronic <luka.furundzija@gmx.at>
2024-01-31 11:35:33 +0100

    Added TimeFreezeService
     Changes to be committed:
    	new file:   src/main/java/com/ardaslegends/service/time/TimeFreezeService.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

---
+++ src/main/java/com/ardaslegends/service/time/TimeFreezeService.java
@@ -0,0 +1,13 @@
+package com.ardaslegends.service.time;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+
+@Slf4j
+@Service
+public class TimeFreezeService {
+    
+}
