be7a04b8983b57388fdcd723d5b1561ee9cf7c34
Luktronic <luka.furundzija@gmx.at>
2024-02-01 13:00:30 +0100

    Started first implementation of start24Timer
     Changes to be committed:
    	modified:   src/main/java/com/ardaslegends/service/time/TimeFreezeService.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/service/time/TimeFreezeService.java
+++ src/main/java/com/ardaslegends/service/time/TimeFreezeService.java
@@ -1,21 +1,43 @@
 package com.ardaslegends.service.time;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+import java.time.Duration;
+import java.util.concurrent.*;
 
 @Slf4j
 @Service
 public class TimeFreezeService {
 
-    private boolean isTimeFrozen = false;
+    private boolean isTimeFrozen;
+
+    private final ExecutorService virtualExecutorService;
+
+    public TimeFreezeService(ExecutorService virtualExecutorService) {
+        isTimeFrozen = false;
+        this.virtualExecutorService = virtualExecutorService;
+    }
+
+    public Future<?> start24hTimer() {
+
+        log.debug("Starting new 24h timer {}", Thread.currentThread());
+        return virtualExecutorService.submit(() -> {
+            log.info("HEELLLOO FROM {}", Thread.currentThread());
+            try {
+                Thread.sleep(Duration.ofSeconds(5));
+                log.info("Timer is over");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+    }
 
     public void freezeTime() {
         isTimeFrozen = true;
     }
-    
+
     public boolean isTimeFrozen() {
         return isTimeFrozen;
     }
