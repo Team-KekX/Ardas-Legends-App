102c3da715d702079ba12c24e07a570c1cfc212b
Luktronic <luka.furundzija@gmx.at>
2024-02-01 13:32:00 +0100

    Tested callback functionality
     Changes to be committed:
    	modified:   src/test/java/com/ardaslegends/service/time/TimeFreezeServiceTest.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/test/java/com/ardaslegends/service/time/TimeFreezeServiceTest.java
+++ src/test/java/com/ardaslegends/service/time/TimeFreezeServiceTest.java
@@ -6,7 +6,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,11 +24,22 @@ public class TimeFreezeServiceTest {
 
     @Test
     public void ensureStart24hTimerWorks() throws InterruptedException {
-        log.debug("{}", Thread.currentThread());
-        val result = timeFreezeService.start24hTimer();
-        Thread.sleep(Duration.ofSeconds(7));
-        log.debug(result.state().name());
-        assertThat(result.isDone()).isTrue();
+        log.debug("Testing if starting a 24h timer works");
+        log.debug("Current Thread: {}", Thread.currentThread());
+        val list = new ArrayList<Integer>();
 
+        val result = timeFreezeService.start24hTimer(() -> list.add(1));
+
+        log.debug("Thread state after starting timer: {}", result.state().name());
+        assertThat(result.isDone()).isFalse();
+        assertThat(result.state()).isEqualTo(Future.State.RUNNING);
+
+        Thread.sleep(Duration.ofSeconds(7));
+
+        log.debug("Thread state after waiting 24h: {}", result.state().name());
+        assertThat(result.isDone()).isTrue();
+        assertThat(list.size()).isEqualTo(1);
+        assertThat(list.get(0)).isEqualTo(1);
+        log.info("Test passed: ensureStart24hTimerWorks");
     }
 }
