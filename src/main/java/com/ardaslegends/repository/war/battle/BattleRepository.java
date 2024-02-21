405793dc619558d55edc0b3c6a9f3f4035c6f641
Luktronic <luka.furundzija@gmx.at>
2024-02-21 22:56:33 +0100

    Implemented custom findByIdOrElseThrow for battle
     Changes to be committed:
    	modified:   src/main/java/com/ardaslegends/repository/war/battle/BattleRepository.java
    	new file:   src/main/java/com/ardaslegends/repository/war/battle/BattleRepositoryCustom.java
    	new file:   src/main/java/com/ardaslegends/repository/war/battle/BattleRepositoryImpl.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/repository/war/battle/BattleRepository.java
+++ src/main/java/com/ardaslegends/repository/war/battle/BattleRepository.java
@@ -3,5 +3,5 @@ package com.ardaslegends.repository.war.battle;
 import com.ardaslegends.domain.war.battle.Battle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BattleRepository extends JpaRepository<Battle, Long> {
+public interface BattleRepository extends JpaRepository<Battle, Long>, BattleRepositoryCustom {
 }
---
+++ src/main/java/com/ardaslegends/repository/war/battle/BattleRepositoryCustom.java
@@ -0,0 +1,8 @@
+package com.ardaslegends.repository.war.battle;
+
+import com.ardaslegends.domain.war.battle.Battle;
+
+public interface BattleRepositoryCustom {
+
+    Battle findByIdOrElseThrow(Long id);
+}
