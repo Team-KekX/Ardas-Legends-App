c9f85c539d2e26f3d76e813156db1131dc197caf
Luktronic <luka.furundzija@gmx.at>
2024-02-21 21:32:23 +0100

    Added UnitCasualty
     Changes to be committed:
    	new file:   src/main/java/com/ardaslegends/domain/war/battle/UnitCasualty.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

---
+++ src/main/java/com/ardaslegends/domain/war/battle/UnitCasualty.java
@@ -0,0 +1,56 @@
+package com.ardaslegends.domain.war.battle;
+
+import com.ardaslegends.domain.Unit;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Objects;
+
+@Embeddable
+public class UnitCasualty {
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "unit_id", foreignKey = @ForeignKey(name = "fk_battle_unit_casualties_unit_id"))
+    private final Unit unit;
+
+    @NotNull
+    private final Long amount;
+
+    public UnitCasualty(Unit unit, Long amount) {
+        Objects.requireNonNull(unit, "UnitCasualty constructor: unit was null!");
+        Objects.requireNonNull(amount, "UnitCasualty constructor: amount was null!");
+
+        this.unit = unit;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "UnitCasualty{" +
+                "unit=" + unit +
+                ", amount=" + amount +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UnitCasualty that = (UnitCasualty) o;
+
+        if (!unit.equals(that.unit)) return false;
+        return amount.equals(that.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = unit.hashCode();
+        result = 31 * result + amount.hashCode();
+        return result;
+    }
+}
