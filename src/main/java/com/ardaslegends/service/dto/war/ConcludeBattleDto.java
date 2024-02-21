6c81f8bfc6d7cb4d337a8d4d51ec2bae0c5dd466
Luktronic <luka.furundzija@gmx.at>
2024-02-21 17:29:30 +0100

    Added battleId to ConcludeBattleDto
     Changes to be committed:
    	modified:   src/main/java/com/ardaslegends/service/dto/war/ConcludeBattleDto.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/service/dto/war/ConcludeBattleDto.java
+++ src/main/java/com/ardaslegends/service/dto/war/ConcludeBattleDto.java
@@ -3,6 +3,7 @@ package com.ardaslegends.service.dto.war;
 import com.ardaslegends.service.dto.player.DiscordIdDto;
 
 public record ConcludeBattleDto(
+        Long battleId,
         String winnerFaction,
         SurvivingUnitsDto[] survivingUnits,
         DiscordIdDto[] playersKilled
