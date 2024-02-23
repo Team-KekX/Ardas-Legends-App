7bd3126e3910b6c2972574cd065c1c6e3907a05e
Luktronic <luka.furundzija@gmx.at>
2024-02-23 14:28:50 +0100

    Changed type of playersKille in ConcludeBattleDto
     Changes to be committed:
    	modified:   src/main/java/com/ardaslegends/service/dto/war/battle/ConcludeBattleDto.java
    
    Signed-off-by: Luktronic <luktronic@gmx.at>

--- src/main/java/com/ardaslegends/service/dto/war/battle/ConcludeBattleDto.java
+++ src/main/java/com/ardaslegends/service/dto/war/battle/ConcludeBattleDto.java
@@ -1,11 +1,9 @@
 package com.ardaslegends.service.dto.war.battle;
 
-import com.ardaslegends.service.dto.player.DiscordIdDto;
-
 public record ConcludeBattleDto(
         Long battleId,
         String winnerFaction,
         SurvivingUnitsDto[] survivingUnits,
-        DiscordIdDto[] playersKilled
+        RpCharCasualtyDto[] playersKilled
 ) {
 }
