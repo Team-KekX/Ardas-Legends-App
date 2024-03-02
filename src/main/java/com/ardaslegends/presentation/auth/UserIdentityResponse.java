19f9a7aaf2935349b00827a365fca2242a4a65ac
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-03-02 10:40:39 +0100

    Add UserIdentityResponse

---
+++ src/main/java/com/ardaslegends/presentation/auth/UserIdentityResponse.java
@@ -0,0 +1,10 @@
+package com.ardaslegends.presentation.auth;
+
+
+/**
+ * This object represents the discord user object
+ * Quite a few fields have been left out, due to them not being needed at the start
+ * For the full list of available fields visit -> https://discord.com/developers/docs/resources/user#user-object
+ */
+public record UserIdentityResponse(String id, String username, Boolean verified) {
+}
