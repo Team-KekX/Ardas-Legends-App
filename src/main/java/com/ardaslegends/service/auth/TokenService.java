6e30746e993e194c1b5781669e85e9b746717898
GroellerKarim <karim.jonas.groeller@gmail.com>
2024-04-30 19:30:14 +0200

    Add TokenService.generateRegistrationToken()

--- src/main/java/com/ardaslegends/service/auth/TokenService.java
+++ src/main/java/com/ardaslegends/service/auth/TokenService.java
@@ -6,5 +6,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 public interface TokenService {
     Player extractPlayer(String token);
     String generateAuthenticationToken(Player player);
+    String generateRegistrationtoken(String discordId);
     boolean isTokenValid(String token, UserDetails userDetails);
 }
